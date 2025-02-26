befa8710c4cb7b70a95c0d6fe3d8e1d7715a4015
Ing-Andy <lefilsdecathy@gmail.com>
2025-02-26 16:15:58 +0100

    j'ai reussie mon fetch

---
+++ Api/ListePokemon.jsx
@@ -0,0 +1,22 @@
+
+import React, { useEffect, useState } from "react";
+
+export default function ListePokemon() {
+  const [pokemon, setPokemon] = useState([]);
+  useEffect(() => {
+    const listeData = async () => {
+      const data = await fetch("https://pokeapi.co/api/v2/pokemon");
+      const liv = await data.json();
+      const results = await Promise.all(
+        liv.results.map(async (pokemon) => {
+          const pokemonResponce = await fetch(pokemon.url);
+          return pokemonResponce.json();
+        })
+      );
+      setPokemon(results);
+    };
+    listeData();
+  }, []);
+  console.log(pokemon);
+  return pokemon;
+}
