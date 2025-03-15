5be02ba004a50e7c3a3e3b523ce6d98024075f96
andy nzoupet <lefilsdecathy@gmail.com>
2025-03-15 15:49:45 +0100

    Update Home.jsx
    j'ai ajouter tout les blocs en fonctions des différents objets que j'ai récupéré dans mon fetch

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import GetPokemon from '../Api/GetPokemon'
 
 export default function Home() {
-    const yo = GetPokemon()
+  const yo = GetPokemon()
+  if(!yo){
+    <p>loading.....</p>
+   }
   return (
     <div>
-        <p> Andy est le meilleur </p>
+       {yo.map(pokemon => (
+         <BlockPokemon nom={pokemon.name} type={pokemon.type} level={pokemon. level} image={pokemon.spreties.other.svg}  />
+       )}
     </div>
   )
 }
